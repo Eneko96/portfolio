0f9727e9c46f60ca60ddd1446e73b2492c9c6e13
Eneko96 <imr.aem2526@gmail.com>
2023-03-20 10:48:20 +0000

    add chevron_right through css

---
+++ test/src/utils/client.ts
@@ -0,0 +1,9 @@
+const VALID_CLIENTS = ['myself', 'hackathon']
+
+export const serializeClient = (clients: string[]): string => {
+  const res = clients.find((client) =>
+    VALID_CLIENTS.includes(client.toLowerCase())
+  )
+
+  return res ?? 'myself'
+}
---
+++ test/src/utils/methodologies.ts
@@ -0,0 +1,16 @@
+const VALID_METHODOLOGIES = [
+  'agile',
+  'scrum',
+  'kanban',
+  'waterfall',
+  'lean',
+  'domain-driven-development'
+]
+
+export const methodologyParser = (methodology: string[]): string => {
+  const res = methodology.find((methodology) =>
+    VALID_METHODOLOGIES.includes(methodology.toLowerCase())
+  )
+  if (res?.includes('-')) res.replaceAll('-', ' ')
+  return res ?? 'agile'
+}
