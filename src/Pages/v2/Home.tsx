75a9344f4eaa1aef71206c8fa2851da4c1d1fd3f
Eneko96 <imr.aem2526@gmail.com>
2023-08-05 02:05:27 +0100

    tmp: add astro notification

--- src/Pages/v2/Home.tsx
+++ src/Pages/v2/Home.tsx
@@ -5,57 +5,71 @@ import { Studies } from '../../Components/v2/Studies/Studies'
 import { HeadLine } from '@Components/v2/HeadLine/HeadLine'
 import { useAppContextV2 } from '@Context/ContextV2'
 import './styles.css'
+import { useEffect, useState } from 'react'
+import Notification from '@TagDs/components/notification/notification'
 
 export const Home = () => {
   const { baseData } = useAppContextV2()
+  const [astroApp, setAstroApp] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!localStorage.getItem('isAstroAdvertised')) setAstroApp(true)
+    }, 5000)
+  })
   if (baseData)
     return (
-      <div role="main">
-        <HeadLine title="Hey welcome to my portfolio!" />
-        <div className="content home-container">
-          <div className="home-hero-container">
-            <div className="home-hero-brief">
-              <h1 className="home-hero-title">
-                I am {baseData.name}, {baseData.position}
-              </h1>
-              <div className="subtitle">Based in</div>
-              <div className="base">Dublin, Ireland</div>
-              <br />
-              <div className="subtitle">
-                Experience In the Following industries
+      <>
+        <div role="main">
+          <HeadLine title="Hey welcome to my portfolio!" />
+          <div className="content home-container">
+            <div className="home-hero-container">
+              <div className="home-hero-brief">
+                <h1 className="home-hero-title">
+                  I am {baseData.name}, {baseData.position}
+                </h1>
+                <div className="subtitle">Based in</div>
+                <div className="base">Dublin, Ireland</div>
+                <br />
+                <div className="subtitle">
+                  Experience In the Following industries
+                </div>
+                <div className="base">{baseData.experience}</div>
+                <br />
+                <div className="subtitle">Core skills</div>
+                <div className="base">{baseData.skills}</div>
+                <br />
+                <div className="subtitle">Cliffton Strengths</div>
+                <div className="base">{baseData.strengths}</div>
               </div>
-              <div className="base">{baseData.experience}</div>
-              <br />
-              <div className="subtitle">Core skills</div>
-              <div className="base">{baseData.skills}</div>
-              <br />
-              <div className="subtitle">Cliffton Strengths</div>
-              <div className="base">{baseData.strengths}</div>
+              <div className="home-hero-image">
+                <picture>
+                  <source
+                    media="(max-width: 768px)"
+                    type="image/png"
+                    srcSet="/home_img_mobile.png"
+                  />
+                  <source type="image/webp" srcSet="/irish.webp" />
+                  <source type="image/jpeg" srcSet="/irish.jpg" />
+                  <img
+                    loading="eager"
+                    alt="head image"
+                    className="header-image"
+                  />
+                </picture>
+              </div>
             </div>
-            <div className="home-hero-image">
-              <picture>
-                <source
-                  media="(max-width: 768px)"
-                  type="image/png"
-                  srcSet="/home_img_mobile.png"
-                />
-                <source type="image/webp" srcSet="/irish.webp" />
-                <source type="image/jpeg" srcSet="/irish.jpg" />
-                <img
-                  loading="eager"
-                  alt="head image"
-                  className="header-image"
-                />
-              </picture>
-            </div>
+            <div className="separator" />
+            <Section />
+            <Studies />
+            <Stack />
+            <CaseStudies />
           </div>
-          <div className="separator" />
-          <Section />
-          <Studies />
-          <Stack />
-          <CaseStudies />
         </div>
-      </div>
+        <Notification show={astroApp} setShow={setAstroApp} info renderAsPortal>
+          Astro version of my portfolio is comming out soon! you can already check it out live in <a href='astro.enekofolio.info'>here</a>
+        </Notification>
+      </>
     )
   return null
 }
