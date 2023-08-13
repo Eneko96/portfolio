f3ede35ce095cfb249eeece4391c65ebd6050ba4
Eneko96 <imr.aem2526@gmail.com>
2023-08-13 19:24:28 +0100

    chore: add phrase for empty readmes

--- src/Pages/v2/Study/lib/getReadme.ts
+++ src/Pages/v2/Study/lib/getReadme.ts
@@ -2,5 +2,8 @@ export const getReadme = async (state: any) => {
   const readme = await fetch(
     `https://raw.githubusercontent.com/${state.owner}/${state.repo}/${state.branch}/README.md`
   )
-  return await readme.text()
+  if (readme.ok) {
+    return await readme.text()
+  }
+  return '<p>Due to unexpected circumstances I was unable to find enough mood to fill this one</p>'
 }
